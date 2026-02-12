cf2f796fe2a055ffef54d57c9f3362c8ee9a9a00
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2026-02-12 14:31:24 -0300

    CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,31 @@ import Button from './components/Button'
 import Numbers from './components/Numbers';
 import { useAppDispatch} from './store/hook'
 import useNumber from './hooks/useNumbers';
-import styles from'./App.module.css';
+import styles from './App.module.css';
+import Input from './components/Input';
 
 const App = () => {
   const {start, erase} = useNumber()
   const dispatch = useAppDispatch()
   const go = () => dispatch(start)
   const clean = () => dispatch(erase)
-  return (
-    <div className={`${styles.body}`}>
-      <header className={`${styles.header}`}>
-        <h1>MEGA-SENA</h1>
-      </header>
-      <div className={`${styles.Appheader}`}>
-        <div>
-          <Numbers />
-          <div>
+  return <div className={`${styles.body}`}>
+   <header className={`${styles.header}`}>
+      <h1>MEGA-SENA</h1>
+   </header>
+   <div className={`${styles.Appheader}`}>
+      <div>
+         <div>
             <Button className={`btn btn-success ${styles.button}`} onClick={go} txt="Sortear"/>
             <Button className={`btn btn-danger ${styles.button}`} onClick={clean} txt="Limpar"/>
-          </div>
-        </div>
+         </div>
       </div>
-    <footer>
+      <Numbers />
+   </div>
+   <footer>
       <small>Feito por Leonardo Marcatti</small>
-    </footer>
-    </div>
-    
-  );
+   </footer>
+</div>
 }
 
-export default App;
+export default App
---
+++ src/components/Input.tsx
@@ -0,0 +1,8 @@
+const Input = () => {
+   return <div className="mb-3" >
+      <label htmlFor="number" className="form-label">Número de Jogos</label>
+      <input type="number" name="number" id="number" className="form-control" />
+   </div>
+}
+
+export default Input
